ba24a337bfc0fdddb6f6c47faa89dee5376c4dff
Feynman Liang <fliang@databricks.com>
2015-08-08 14:27:59 -0700

    Rebase master

--- server/ReservationsCronJob.js
+++ server/ReservationsCronJob.js
@@ -2,15 +2,22 @@
 SyncedCron.add({
   name: 'Updates Reservations queues',
   schedule: function(parser) {
+    // TODO: change to actual cron interval
     return parser.text('every 5 seconds');
   },
   job: function() {
     var reservationsByCreatedAt =
       Reservations.find({}, { $orderby: { createdAt: -1} });
     if (reservationsByCreatedAt.length !== 0) {
-      var reservationToRemove = reservationsByCreatedAt.fetch()[0];
-      console.log("Removing " + reservationToRemove._id);
-      Reservations.remove(reservationToRemove._id);
+      var firstReservation = reservationsByCreatedAt.fetch()[0];
+      var timeElapsed =
+        moment.duration(moment() - moment(firstReservation.createdAt));
+
+      // TODO: change to actual bathroom duration time
+      if (timeElapsed > moment.duration(15, 'seconds')) {
+        console.log("Removing " + firstReservation._id);
+        Reservations.remove(firstReservation._id);
+      }
     } else {
       console.log("No reservations to remove!");
     }
