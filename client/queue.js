44ff27e36bfd54be3a07563358354bd62906f30c
nflavour <nflavour@gmail.com>
2015-08-08 15:56:41 -0700

    moved joinQueue to separate file

--- client/queue.js
+++ client/queue.js
@@ -9,14 +9,3 @@ Template.queueItem.helpers({
     return moment(this.createdAt).format();
   }
 });
-
-Template.joinQueue.events({
-    "click": function(event) {
-        event.preventDefault();
-        Reservations.insert({
-          userId: Math.floor(Math.random() * 1000) + 1,
-          createdAt: moment().valueOf(),
-          status: "Active"
-        });
-    }
-})
