0a62a65f79f9970c4d656bd7a3f32fccd414673e
Feynman Liang <fliang@databricks.com>
2015-08-09 11:50:31 -0700

    Faster cron, slower timeout

--- PayToPottie.js
+++ PayToPottie.js
@@ -4,8 +4,8 @@ if (Meteor.isServer) {
   Bathrooms._ensureIndex({"loc" : "2dsphere"});
 }
 
-CRON_INTERVAL = 'every 5 seconds';
-BATHROOM_TIME_SECONDS = 10;
+CRON_INTERVAL = 'every 1 seconds';
+BATHROOM_TIME_SECONDS = 30;
 COUNTDOWN_TIME_MILLIS = 200;
 
 // TODO: make this a pub-sub so bathrooms that are added are reactively updated
