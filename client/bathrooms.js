52adffdb31e266fb8b4cb60a063c34e89f3a25f4
nflavour <nflavour@gmail.com>
2015-08-08 23:53:28 -0700

    added select marker event handling

--- client/bathrooms.js
+++ client/bathrooms.js
@@ -13,6 +13,26 @@ Template.bathrooms.events({
 
 Template.indBathroom.helpers({
   selectedMarkerStyle: function() {
-    return  Session.equals('selectedMarker', this._id) ? 'active' : '';
+    console.log("selected marker", this._id);
+    if(Session.equals('selectedMarker', this._id)) {
+      console.log("activate", this._id);
+      $('.panel-collapse').collapse('hide');
+      $('#collapse-' + this._id).collapse('show');
+      return 'marked';
+    } else {
+      return '';
+    }
+  },
+  abc: function() {
+    console.log("Abc");
   }
 });
+
+// $('.panel-collapse').collapse('hide');
+Template.indBathroom.events({
+  'click .bathroom-item': function(event) {
+    var bathroomId = event.currentTarget.id;
+    console.log("You clicked on", bathroomId);
+    Session.set("selectedMarker", bathroomId);
+  }
+})
