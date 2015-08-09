86bfdbe8ff89bc1b31fdf45610d8916f1e9b2054
nflavour <nflavour@gmail.com>
2015-08-09 01:09:37 -0700

    add some styles

--- client/bathrooms.js
+++ client/bathrooms.js
@@ -11,25 +11,14 @@ Template.bathrooms.events({
 })
 
 Template.indBathroom.helpers({
-  selectedMarkerStyle: function() {
-    console.log("selected marker", this._id);
-    if(Session.equals('selectedMarker', this._id)) {
-      console.log("activate", this._id);
-      $('.panel-collapse').collapse('hide');
-      $('#collapse-' + this._id).collapse('show');
-      return 'marked';
-    } else {
-      return '';
-    }
+  hovereMarkerStyle: function() {
+    return Session.equals('hoveredMarker', this._id) ? 'active' : '';
   }
 });
 
-// $('.panel-collapse').collapse('hide');
 Template.indBathroom.events({
   'click .bathroom-item': function(event) {
-    var bathroomId = event.currentTarget.id;
-    console.log("You clicked on", bathroomId);
-    Session.set("selectedMarker", bathroomId);
+    toggleBathroomPanelBody(event.currentTarget.id);
   }
 })
 
