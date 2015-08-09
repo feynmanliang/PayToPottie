24f56eab4679494388d53121264127ce584b743d
nflavour <nflavour@gmail.com>
2015-08-08 17:30:57 -0700

    cleaned up list of bathrooms

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,6 +1,6 @@
   Meteor.startup(function () {
     if (Bathrooms.find().count() === 0) {
-      Bathrooms.insert({
+      var bathroomId1 = Bathrooms.insert({
         // TODO: GPS coords for bathroom
         // TODO: Photos for bathrooms
         name: "My awesome toilet",
@@ -8,25 +8,39 @@
         price: 2.55,
         owner: 11
       });
+      var bathroomId2 = Bathrooms.insert({
+        // TODO: GPS coords for bathroom
+        // TODO: Photos for bathrooms
+        name: "My dirty toilet",
+        description: "explosive diarrhea",
+        price: 0.50,
+        owner: 12
+      });
     }
     if (Reservations.find().count() === 0) {
-      var bathroomId = Bathrooms.findOne()._id;
       Reservations.insert({
         userId: 123,
-        bathroomId: bathroomId,
+        bathroomId: bathroomId1,
         createdAt: moment().valueOf(),
       });
 
       Reservations.insert({
         userId: 456,
-        bathroomId: bathroomId,
+        bathroomId: bathroomId1,
         createdAt: moment().valueOf() + 1000,
       });
 
       Reservations.insert({
         userId: 789,
-        bathroomId: bathroomId,
+        bathroomId: bathroomId1,
         createdAt: moment().valueOf() + 2000,
       });
+    }
+    for(var i = 0; i < 100; i++) {
+      Reservations.insert({
+        userId: Math.floor(Math.random() * 1000 + 1),
+        bathroomId: bathroomId2,
+        createdAt: moment().valueOf() + 1000 * i
+      })
     }
   });
