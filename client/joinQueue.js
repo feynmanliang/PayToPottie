0b67b781769f106329f93d80e2ef7c323437f704
Niole Nelson <niolenelson@gmail.com>
2015-08-09 00:35:07 -0400

    can't join same queue more than once

--- client/joinQueue.js
+++ client/joinQueue.js
@@ -2,14 +2,26 @@ Template.joinQueue.events({
   "click .joinQueue": function(event) {
     event.preventDefault();
 
+
     var currId = Meteor.userId();
+    var currQ = Reservations.find({bathroomId: this._id});
+    var canJoin = true;
+
+    currQ.forEach(function(customer) {
+      if (customer.userId === currId) {
+        canJoin = false;
+      }
+    });
+
+
     var newReservation = {
       userId: currId,
       createdAt: moment().valueOf(),
       bathroomId: this._id,
       status: "Active"
     };
-
-    Reservations.insert(newReservation);
+    if (canJoin) {
+      Reservations.insert(newReservation);
+    }
    }
 });
