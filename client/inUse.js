dfdb4b6517f6d0eaf1a79e662477e171a6675a30
Feynman Liang <fliang@databricks.com>
2015-08-09 09:08:25 -0700

    Correct time until in inUse

--- client/inUse.js
+++ client/inUse.js
@@ -1,6 +1,10 @@
 Template.inUse.helpers({
   endTime: function() {
-    console.log(this);
+    return (
+      moment(
+        moment(this.beganProcessingAt)
+        + moment.duration(BATHROOM_TIME_SECONDS, 'seconds')
+      ).format('hh:mm'));
   }
 });
 
