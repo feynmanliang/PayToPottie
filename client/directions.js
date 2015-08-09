4a1b31ac9e8e537bbc1b8d9b38ca07720004ada3
Feynman Liang <fliang@databricks.com>
2015-08-09 07:23:49 -0700

    Fix direciton bug

--- client/directions.js
+++ client/directions.js
@@ -13,26 +13,31 @@ Template.directions.helpers({
 });
 
 Template.directions.onRendered(function() {
-  if (this.data) {
-    var pos = Location.getLastPosition();
-    var lonLat = this.data.loc.coordinates;
-    GoogleMaps.ready('directionsMap', function(map) {
-      var directionsService = new google.maps.DirectionsService();
-      var directionsDisplay = new google.maps.DirectionsRenderer();
-      directionsDisplay.setMap(map.instance);
+  var self = this;
+  Tracker.autorun(function() {
+    console.log(this);
+    if (this.data) {
+      console.log(this);
+      var pos = Location.getLastPosition();
+      var lonLat = this.data.loc.coordinates;
+      GoogleMaps.ready('directionsMap', function(map) {
+        var directionsService = new google.maps.DirectionsService();
+        var directionsDisplay = new google.maps.DirectionsRenderer();
+        directionsDisplay.setMap(map.instance);
 
-      var start = new google.maps.LatLng(pos.latitude, pos.longitude);
-      var end = new google.maps.LatLng(lonLat[1], lonLat[0]);
-      var request = {
-        origin:start,
-        destination:end,
-        travelMode: google.maps.TravelMode.WALKING
-      };
-      directionsService.route(request, function(result, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          directionsDisplay.setDirections(result);
-        }
+        var start = new google.maps.LatLng(pos.latitude, pos.longitude);
+        var end = new google.maps.LatLng(lonLat[1], lonLat[0]);
+        var request = {
+          origin:start,
+          destination:end,
+          travelMode: google.maps.TravelMode.WALKING
+        };
+        directionsService.route(request, function(result, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+          }
+        });
       });
-    });
-  }
+    }
+  }.bind(this));
 });
